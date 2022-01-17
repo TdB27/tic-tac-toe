f74e288d554b3c2f04d16c9fa75803cdac6a3324
Thiago Billy <thiagobilly27@gmail.com>
2022-01-17 12:57:12 -0300

    logica do jogo

---
+++ assets/scripts/main.js
@@ -0,0 +1,106 @@
+const game = function () {
+  let card = document.querySelectorAll('[data-card]')
+  let player,
+    vencedor = null
+
+  selectPlayer('O', '')
+
+  function initGame() {
+    card.forEach(item => {
+      item.addEventListener('click', () => {
+        match(item)
+      })
+    })
+  }
+
+  function match(value) {
+    if (value.innerHTML !== '' || vencedor !== null) return
+
+    if (player === 'O') {
+      player = 'X'
+    } else {
+      player = 'O'
+    }
+
+    selectPlayer(player, value)
+    checkWin()
+  }
+
+  function selectPlayer(value, print) {
+    player = value
+    print.innerHTML = player
+  }
+
+  function checkWin() {
+    const card1 = document.getElementById('card1')
+    const card2 = document.getElementById('card2')
+    const card3 = document.getElementById('card3')
+    const card4 = document.getElementById('card4')
+    const card5 = document.getElementById('card5')
+    const card6 = document.getElementById('card6')
+    const card7 = document.getElementById('card7')
+    const card8 = document.getElementById('card8')
+    const card9 = document.getElementById('card9')
+
+    if (sequence(card1, card2, card3)) {
+      activeWin(card1, card2, card3)
+      changeWin(card1)
+    }
+    if (sequence(card4, card5, card6)) {
+      activeWin(card4, card5, card6)
+      changeWin(card4)
+    }
+    if (sequence(card7, card8, card9)) {
+      activeWin(card7, card8, card9)
+      changeWin(card7)
+    }
+    if (sequence(card1, card4, card7)) {
+      activeWin(card1, card4, card7)
+      changeWin(card1)
+    }
+    if (sequence(card2, card5, card8)) {
+      activeWin(card2, card5, card8)
+      changeWin(card2)
+    }
+    if (sequence(card3, card6, card9)) {
+      activeWin(card3, card6, card9)
+      changeWin(card3)
+    }
+    if (sequence(card1, card5, card9)) {
+      activeWin(card1, card5, card9)
+      changeWin(card1)
+    }
+    if (sequence(card3, card5, card7)) {
+      activeWin(card3, card5, card7)
+      changeWin(card3)
+    }
+  }
+
+  function sequence(card1, card2, card3) {
+    let isEqual = false
+    if (
+      card1.innerHTML !== '' &&
+      card1.innerHTML === card2.innerHTML &&
+      card1.innerHTML === card3.innerHTML
+    ) {
+      isEqual = true
+    }
+
+    return isEqual
+  }
+
+  function changeWin(card) {
+    vencedor = card.innerHTML
+  }
+
+  function activeWin(card1, card2, card3) {
+    card1.classList.add('card-active')
+    card2.classList.add('card-active')
+    card3.classList.add('card-active')
+  }
+
+  return { initGame }
+}
+
+const newGame = game()
+newGame.initGame()
